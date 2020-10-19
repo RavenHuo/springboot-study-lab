15caa3db704519cd5fd6a5e65ed2b8419874b240
raven <huoruiwen@rongshantech.com>
2020-10-19 12:02:50 +0800

    定义zuul 对称加密sdk

---
+++ api-encryption-zuul/src/main/java/com/raven/api/encryption/ApiEncryptionZuulAutoConfiguration.java
@@ -0,0 +1,34 @@
+package com.raven.api.encryption;
+
+import com.raven.api.encryption.common.ApiEncryptionConstant;
+import com.raven.api.encryption.zuul.ApiEncryptionZuulFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @description:
+ * @author: huorw
+ * @create: 2020-10-12 18:59
+ */
+@ConditionalOnProperty(value = ApiEncryptionConstant.SPRING_API_ENCRYPTION_SWITCH, matchIfMissing = true)
+@Configuration
+@ComponentScan({"com.raven.api.encryption"})
+@Slf4j
+public class ApiEncryptionZuulAutoConfiguration {
+
+    @PostConstruct
+    public void init() {
+        log.info("ApiEncryptionAutoConfiguration init------------------------");
+    }
+
+    @Bean
+    public ApiEncryptionZuulFilter feignInterceptor() {
+        return new ApiEncryptionZuulFilter();
+    }
+
+}
