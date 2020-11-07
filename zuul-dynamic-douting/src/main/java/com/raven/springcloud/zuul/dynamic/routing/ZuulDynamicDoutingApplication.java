305b901f747e4fd920911ab44e74f2fe3ce5fd64
raven <huoruiwen@rongshantech.com>
2020-11-07 16:22:28 +0800

    zuul 动态路由配置

---
+++ zuul-dynamic-douting/src/main/java/com/raven/springcloud/zuul/dynamic/routing/ZuulDynamicDoutingApplication.java
@@ -0,0 +1,13 @@
+package com.raven.springcloud.zuul.dynamic.routing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulDynamicDoutingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulDynamicDoutingApplication.class, args);
+    }
+
+}
---
+++ zuul-dynamic-douting/src/test/java/com/raven/springcloud/zuul/dynamic/routing/ZuulDynamicDoutingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raven.springcloud.zuul.dynamic.routing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulDynamicDoutingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
